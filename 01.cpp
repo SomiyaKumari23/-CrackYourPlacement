417dfc1ef759086b194751f1e24d7610c0ab2472
SomiyaKumari23 <93765781+SomiyaKumari23@users.noreply.github.com>
2024-07-16 09:10:39 +0530

    Two Sum

--- 01.cpp
+++ 01.cpp
@@ -1,21 +1,15 @@
-
 class Solution {
 public:
-    int findDuplicate(vector<int>& nums) {
-       int slow=nums[0];
-        int fast=nums[0];
-      do {
-            slow = nums[slow];
-            fast = nums[nums[fast]];
-        } while (slow != fast);
-       cout<<slow<<" ";
-        slow=nums[0];
-        while (slow != fast) {
-            slow = nums[slow];
-            fast = nums[fast];
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> mp;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]=i;
         }
-
-        return slow;
-   
+        for(int i=0;i<nums.size();i++){
+            if(mp.find(target-nums[i])!=mp.end() && i!=mp[target-nums[i]]){
+                return {i,mp[target-nums[i]]};
+            }
+        }
+        return {};
     }
 };
