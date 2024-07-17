91d881358e47b1987d69d96c6fe7e8f95d7c1bfa
SomiyaKumari23 <93765781+SomiyaKumari23@users.noreply.github.com>
2024-07-17 23:23:25 +0530

    Best Time to Buy and Sell Stock II

--- 03.cpp
+++ 03.cpp
@@ -1,17 +1,24 @@
-
 class Solution {
 public:
-    void moveZeroes(vector<int>& nums) {
-        int j=0;
-        for(int i=0;i<nums.size();i++){
-             if(nums[i]!=0){
-                  swap(nums[j],nums[i]);
-                j++;
-             }
-           }
-
+    int func(vector<int> &prices,int buy,int ind,vector<vector<int>> &dp){
+        if(ind>=prices.size())
+        return 0;
+        if(dp[ind][buy]!=-1)
+        return dp[ind][buy];
+        int take,nottake;
+        if(buy){
+            take=-prices[ind]+func(prices,0,ind+1,dp);
+            nottake=func(prices,1,ind+1,dp);
         }
-          
-        
-    
+        else{
+            take=prices[ind]+func(prices,1,ind+1,dp);
+            nottake=func(prices,0,ind+1,dp);
+        }
+        return dp[ind][buy]=max(take,nottake);
+    }
+
+    int maxProfit(vector<int>& prices) {
+        vector<vector<int>> dp(prices.size()+1,vector<int>(2,-1));
+        return func(prices,1,0,dp);
+    }
 };
